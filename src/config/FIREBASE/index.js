ac74d9f4593954d0f25ecfd99579c35321d02d90
Dimas Khulil Arifin <dimaskarifin@gmail.com>
2022-07-29 23:12:08 +0700

    update AuthAction,reducer,set firebase

---
+++ src/config/FIREBASE/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCg1kcscYGFX5OCIOU5zY1B1e-BBu8cgA8',
+  authDomain: 'tukatuku-bcf28.firebaseapp.com',
+  databaseURL:
+    'https://tukatuku-bcf28-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'tukatuku-bcf28',
+  storageBucket: 'tukatuku-bcf28.appspot.com',
+  messagingSenderId: '1059704877096',
+  appId: '1:1059704877096:web:c4605344034cbd2669851d',
+});
+
+const FIREBASE = firebase;
+export default FIREBASE;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from "redux";
 import CatHoodieReducer from "./cathoodie";
+import AuthReducer from "./auth";
+import HoodieReducer from "./hoodie";
+import PesananReducer from "./pesanan";
 
 export default combineReducers({
   CatHoodieReducer,
+  AuthReducer,
+  HoodieReducer,
+  PesananReducer,
 });
